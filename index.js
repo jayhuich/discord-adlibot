2c402063b37377eb08e189d722c4c7af8652cff9
jayhuich <29498758+jay-hui@users.noreply.github.com>
2022-08-11 14:56:55 +0800

    cleanup

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const fs = require('node:fs');
 const path = require('node:path');
 const { Client, Collection, GatewayIntentBits } = require('discord.js');
-const { createAudioPlayer, NoSubscriberBehavior, AudioPlayerStatus } = require('@discordjs/voice');
 
 const { token } = require('./config.json');
 
